3b286c714c808dd0cb687efa156955332249ff09
cschaefer26 <c.schaefer.home@gmail.com>
2020-05-01 15:12:58 +0200

    normalize

--- utils/distribution.py
+++ utils/distribution.py
@@ -253,7 +253,7 @@ class TacotronSTFT(torch.nn.Module):
         magnitudes, phases = self.stft_fn.transform(y)
         #magnitudes = magnitudes.data
         mel_output = torch.matmul(self.mel_basis, magnitudes)
-        #mel_output = self.spectral_normalize(mel_output)
+        mel_output = self.spectral_normalize(mel_output)
         return mel_output
         #return mel_output
 
