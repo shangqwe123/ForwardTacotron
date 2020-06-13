a2db9ea12cfca24c2b940216202b88867af05d65
cschaefer26 <c.schaefer.home@gmail.com>
2020-06-13 19:00:14 +0200

    Adjusted sigma.

--- models/forward_tacotron.py
+++ models/forward_tacotron.py
@@ -153,7 +153,7 @@ class ForwardTacotron(nn.Module):
         x = torch.zeros((bs, mel_len, x_p.shape[-1])).to(device)
         for t in range(mel_len):
             t_tens = torch.full((bs, 1), fill_value=t).to(device)
-            wt = torch.exp(-0.1*(t_tens - mids) ** 2)
+            wt = torch.exp(-1.*(t_tens - mids) ** 2)
             norm = torch.sum(wt, dim=1) + 1e-10
             norm = norm.unsqueeze(-1)
             wt = wt.unsqueeze(-1)
