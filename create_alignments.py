Fixed scaling.

create_alignments.py
@@ -14,8 +14,8 @@ from utils import hparams as hp
 hp.configure('hparams.py')  # Load hparams from file
 
 text_dict = unpickle_binary('data/text_dict.pkl')
-mel = np.load('data/mel/02075.npy')
-text = text_dict['02075']
+mel = np.load('data/mel/00092.npy')
+text = text_dict['00092']
 
 device = torch.device('cpu')
 model = Aligner(n_mels=80, lstm_dim=256, num_symbols=len(phonemes)).to(device)