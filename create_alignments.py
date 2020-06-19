Fixed scaling.

create_alignments.py
@@ -20,7 +20,7 @@ text = text_dict['00092']
 device = torch.device('cpu')
 model = Aligner(n_mels=80, lstm_dim=256, num_symbols=len(phonemes)).to(device)
 model.eval()
-model.load('checkpoints/asvoice_newdurs_tts.aligner/latest_weights.pyt')
+model.load('checkpoints/asvoice_aligner_melgan_tts.aligner/latest_weights.pyt')
 
 print(f'loaded aligner step {model.get_step()}')
 mel = torch.tensor(mel)