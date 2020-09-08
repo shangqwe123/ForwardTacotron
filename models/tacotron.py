d599769f82ac8a7ed024c58594fd1d41901fe58d
cschaefer26 <c.schaefer.home@gmail.com>
2020-09-08 13:45:36 +0200

    Masking after 50K steps.

--- models/tacotron.py
+++ models/tacotron.py
@@ -348,6 +348,8 @@ class Tacotron(nn.Module):
         mel_outputs, attn_scores = [], []
         b, _, t_len = m.size()
         if self.training and self.step > 50_000:
+            if self.step == 50_000:
+                print('Switching to mask!')
             mask = (torch.rand(b, t_len, device=device) < 0.5).float()
             avg_m = torch.mean(m)
             m = m * mask[:, None, :] + avg_m * (1. - mask[:, None, :])
